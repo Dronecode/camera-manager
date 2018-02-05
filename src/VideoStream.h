8afe0db970886c6865bd62d003eee9da1a7bad0a
lbegani <lalit.kumar.begani@intel.com>
2018-02-05 13:58:33 +0530

    Add UDP Video Streaming Feature

---
+++ src/VideoStream.h
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the Camera Streaming Daemon
+ *
+ * Copyright (C) 2018  Intel Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+class VideoStream {
+public:
+    VideoStream() {}
+    ~VideoStream() {}
+
+    enum State { STATE_ERROR = -1, STATE_IDLE = 0, STATE_INIT = 1, STATE_RUN = 2 };
+
+    virtual int init() = 0;
+    virtual int uninit() = 0;
+    virtual int start() = 0;
+    virtual int stop() = 0;
+    virtual int getState() = 0;
+    virtual int setResolution(int imgWidth, int imgHeight) = 0;
+    virtual int setFormat(int vidFormat) = 0;
+};
