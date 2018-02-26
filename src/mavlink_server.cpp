Dronecode/camera-manager

commit 5672f178b03cb3091533af8e254e93c662f77f5c

Implement reset camera parameters

src/mavlink_server.cpp
@@ -518,6 +518,26 @@ void MavlinkServer::_handle_param_ext_set(const struct sockaddr_in &addr, mavlin
     }
 }
 
+void MavlinkServer::_handle_reset_camera_settings(const struct sockaddr_in &addr,
+                                                  mavlink_command_long_t &cmd)
+{
+
+    if (cmd.param1 != 1) {
+        _send_ack(addr, cmd.command, cmd.target_component, true);
+        return;
+    }
+
+    bool success = false;
+    CameraComponent *tgtComp = getCameraComponent(cmd.target_component);
+
+    if (tgtComp) {
+        if (!tgtComp->resetCameraSettings())
+            success = true;
+    }
+
+    _send_ack(addr, cmd.command, cmd.target_component, success);
+}
+
 void MavlinkServer::_handle_mavlink_message(const struct sockaddr_in &addr, mavlink_message_t *msg)
 {
     // log_debug("Message received: (sysid: %d compid: %d msgid: %d)", msg->sysid, msg->compid,
@@ -551,7 +571,7 @@ void MavlinkServer::_handle_mavlink_message(const struct sockaddr_in &addr, mavl
             this->_handle_request_camera_capture_status(addr, cmd);
             break;
         case MAV_CMD_RESET_CAMERA_SETTINGS:
-            log_debug("MAV_CMD_RESET_CAMERA_SETTINGS");
+            this->_handle_reset_camera_settings(addr, cmd);
             break;
         case MAV_CMD_REQUEST_STORAGE_INFORMATION:
             this->_handle_request_storage_information(addr, cmd);

src/CameraComponent.cpp
@@ -302,6 +302,14 @@ int CameraComponent::setCameraMode(uint32_t mode)
     return 0;
 }
 
+int CameraComponent::resetCameraSettings()
+{
+    int ret = mCamDev->resetParams(mCamParam);
+    if (ret != 0)
+        log_debug("Error in reset of camera parameters. Could not open the device.");
+    return ret;
+}
+
 int CameraComponent::getCameraMode()
 {
     return mCamDev->getMode();

src/CameraComponent.h
@@ -77,6 +77,7 @@ public:
     virtual int stopImageCapture();
     void cbImageCaptured(int result, int seq_num);
     virtual int setImageLocation(std::string imgPath);
+    int resetCameraSettings(void);
 
 private:
     std::string mCamDevName;               /* Camera device name */

src/CameraDevice.h
@@ -51,7 +51,7 @@ public:
     virtual int start(std::function<void(std::vector<uint8_t>)> cb) { return -ENOTSUP; }
     virtual int stop() { return -ENOTSUP; }
     virtual std::vector<uint8_t> read() { return {}; }
-    virtual int resetParams() { return -ENOTSUP; }
+    virtual int resetParams(CameraParameters &camParam) { return -ENOTSUP; }
     virtual int setParam(const char *param_id, size_t id_size, const char *param_value,
                          size_t value_size, int param_type) { return -ENOTSUP; }
     virtual int setParam(std::string param_id, float param_value) { return -ENOTSUP; }

src/CameraDeviceV4l2.cpp
@@ -17,6 +17,7 @@
  */
 #include <cstring>
 #include <linux/videodev2.h>
+#include <sys/ioctl.h>
 
 #include "CameraDeviceV4l2.h"
 #include "v4l2_interface.h"
@@ -127,6 +128,69 @@ int CameraDeviceV4l2::setMode(uint32_t mode)
 int CameraDeviceV4l2::getMode()
 {
     return mMode;
+}
+
+int CameraDeviceV4l2::resetParams(CameraParameters &camParam)
+{
+    int ret = 0;
+    struct v4l2_queryctrl queryctrl;
+
+    int fd = v4l2_open(mDeviceId.c_str());
+
+    if (fd == -1) {
+        log_debug("Error in opening camera device in resetParams()");
+        ret = 1;
+        return ret;
+    }
+
+    memset(&queryctrl, 0, sizeof(queryctrl));
+
+    for (queryctrl.id = V4L2_CID_BASE; queryctrl.id < V4L2_CID_LASTP1; queryctrl.id++) {
+        if (0 == ioctl(fd, VIDIOC_QUERYCTRL, &queryctrl)) {
+            if (queryctrl.flags & V4L2_CTRL_FLAG_DISABLED)
+                continue;
+        }
+        int ret = v4l2_set_control(fd, queryctrl.id, queryctrl.default_value);
+        //    log_debug("return value for:%s : %d : %d",queryctrl.name,queryctrl.id,ret);
+
+        switch (queryctrl.id) {
+        case V4L2_CID_BRIGHTNESS:
+            camParam.setParameter(CameraParameters::BRIGHTNESS, (uint32_t)queryctrl.default_value);
+            break;
+        case V4L2_CID_CONTRAST:
+            camParam.setParameter(CameraParameters::CONTRAST, (uint32_t)queryctrl.default_value);
+            break;
+        case V4L2_CID_WHITE_BALANCE_TEMPERATURE:
+            camParam.setParameter(CameraParameters::WHITE_BALANCE_TEMPERATURE,
+                                  (uint32_t)queryctrl.default_value);
+            break;
+        case V4L2_CID_SATURATION:
+            camParam.setParameter(CameraParameters::SATURATION, (uint32_t)queryctrl.default_value);
+            break;
+        case V4L2_CID_HUE:
+            camParam.setParameter(CameraParameters::HUE, (int32_t)queryctrl.default_value);
+            break;
+        case V4L2_CID_EXPOSURE:
+            camParam.setParameter(CameraParameters::EXPOSURE_ABSOLUTE,
+                                  (uint32_t)queryctrl.default_value);
+            break;
+        case V4L2_CID_GAIN:
+            camParam.setParameter(CameraParameters::GAIN, (uint32_t)queryctrl.default_value);
+            break;
+        case V4L2_CID_POWER_LINE_FREQUENCY:
+            camParam.setParameter(CameraParameters::POWER_LINE_FREQ_MODE,
+                                  (uint32_t)queryctrl.default_value);
+            break;
+        case V4L2_CID_SHARPNESS:
+            camParam.setParameter(CameraParameters::SHARPNESS, (uint32_t)queryctrl.default_value);
+            break;
+            //     case V4L2_CID_AUTOGAIN:
+            //     case V4L2_CID_HFLIP:
+            //     case V4L2_CID_VFLIP:
+        }
+    }
+    //    log_debug("Reset Done!");
+    return ret;
 }
 
 int CameraDeviceV4l2::setBrightness(uint32_t value)

src/CameraDeviceV4l2.h
@@ -51,6 +51,7 @@ public:
     int setExposureAbsolute(uint32_t value);
     int setSceneMode(uint32_t value);
     int setHue(int32_t value);
+    int resetParams(CameraParameters &camParam);
 
 private:
     std::string mDeviceId;

src/mavlink_server.h
@@ -77,6 +77,7 @@ private:
     void _handle_param_ext_request_read(const struct sockaddr_in &addr, mavlink_message_t *msg);
     void _handle_param_ext_request_list(const struct sockaddr_in &addr, mavlink_message_t *msg);
     void _handle_param_ext_set(const struct sockaddr_in &addr, mavlink_message_t *msg);
+    void _handle_reset_camera_settings(const struct sockaddr_in &addr, mavlink_command_long_t &cmd);
     bool _send_mavlink_message(const struct sockaddr_in *addr, mavlink_message_t &msg);
     void _send_ack(const struct sockaddr_in &addr, int cmd, int comp_id, bool success);
     const Stream::FrameSize *_find_best_frame_size(Stream &s, uint32_t w, uint32_t v);

commit 2730c7faa2d0f6220ad80bead6794a94d793b7eb

Fix build with "--enable-aero"
5672f178 broke the build due to string split without \.

src/mavlink_server.cpp
@@ -66,7 +66,7 @@ MavlinkServer::MavlinkServer(ConfFile &conf, std::vector<std::unique_ptr<Stream>
 
     if (opt.sysid) {
         if (opt.sysid < 1 || opt.sysid >= 255)
-            log_error("Invalid System ID for MAVLink communication (%d).Waiting for heartbeat from
+            log_error("Invalid System ID for MAVLink communication (%d).Waiting for heartbeat from \
              Vehicle",opt.sysid);
         else
             _system_id = opt.sysid;